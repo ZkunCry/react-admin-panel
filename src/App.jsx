1409b9659e77442801d5dfc495448448331f7cc7
ZkunCry <aksentev.03@mail.ru>
2024-06-03 23:36:12 +0500

    installed redux,redux-toolkit and created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { SignIn, Wrapper } from "./components";
+import { Main } from "./pages";
+import PrivateRouter from "./utils/routes/privateRouter";
+import PublicRouter from "./utils/routes/publicRouter";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Wrapper />}>
-        <Route path="signin" element={<SignIn />} />
+        <Route element={<PublicRouter />}>
+          <Route path="signin" element={<SignIn />} />
+        </Route>
+
+        <Route element={<PrivateRouter />}>
+          <Route path="main" element={<Main />} />
+        </Route>
       </Route>
     </Routes>
   );
